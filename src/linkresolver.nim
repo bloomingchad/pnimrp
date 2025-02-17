3c076d94bd45ef5e20a204a7b94883911272947a
bloomingchad <antonloyed007@gmail.com>
2025-02-17 21:44:06 +0530

    async-stationstatus: make linkresolver and resolveLinkSync async
    - add async pragma to resolveLinkSync
    - Changes the return type of `resolveLinkSync` in `linkresolver.nim` from `LinkStatus` to `Future[LinkStatus]`
    - change `sleep 20` to `await sleepAsync 20`

--- src/linkresolver.nim
+++ src/linkresolver.nim
@@ -1,15 +1,15 @@
 # linkresolver.nim
 
-import times, random, utils, os
+import times, random, utils, os, asyncdispatch
 
-proc resolveLinkSync*(url: string): LinkStatus =
+proc resolveLinkSync*(url: string): Future[LinkStatus] {.async.} =
   ## Simulates link resolution with 80% valid, 20% invalid ratio
   randomize()
   
   # Simulate delay (unchanged)
   #let delayMs = 1000 + rand(500) - rand(500)
   #sleep(delayMs)
-  sleep 20
+  await sleepAsync 20
 
   # Return lsValid for 80% of cases (80/100 = 80%)
   if rand(99) < 80:  # 0-79 (80 values) = valid
