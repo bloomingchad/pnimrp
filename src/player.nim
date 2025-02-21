2fca1a0e0a4c09e501bfe12df472592162931d47
bloomingchad <antonloyed007@gmail.com>
2025-02-21 20:06:59 +0530

    edge-case: player: rm default key bindings and vid output
    this commit disables the default libmpv key bindings and video output to prevent conflicts with the apps custom key handling and to ensure a consistent audio-only experience.

--- src/player.nim
+++ src/player.nim
@@ -39,6 +39,8 @@ proc init*(ctx: ptr Handle, source: string) {.raises: [PlayerError].} =
 
     var oscEnabled = cint(1)
     cE ctx.setOption("osc", fmtFlag, addr oscEnabled)
+    cE(ctx.setOptionString("input-default-bindings", "no"))
+    cE(ctx.setOptionString("input-vo-keyboard", "no"))
     cE ctx.setPropertyString("vid", "no")
     cE initialize(ctx)
     cE ctx.cmd(fileArgs)
