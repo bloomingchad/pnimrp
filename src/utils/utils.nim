1997bcad292fc2538b505adfe12c93464c421cb6
bloomingchad <antonloyed007@gmail.com>
2025-03-01 19:09:03 +0530

    refactor: utils: simplify `MenuChars` constant
        - replace sequence of chars with a string literal
    
    the `MenuChars` constant is simplified by using a string literal instead of a sequence of individual character literals. this improves readability and reduces code verbosity without altering functionality

--- src/utils/utils.nim
+++ src/utils/utils.nim
@@ -124,12 +124,7 @@ var
   scrollCounter* = 0
 
 const
-  MenuChars* = @[
-    '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-  ]
+  MenuChars* = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   AppName* = "Poor Mans Radio Player"
   AppNameShort* = "PNimRP"
   DefaultErrorMsg* = "INVALID CHOICE"
