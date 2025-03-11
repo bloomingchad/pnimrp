e9edb4148f3f2615766bf964ffa1d70fcb87e049
bloomingchad <enzocassio2@imap.fi>
2025-03-11 20:06:22 +0530

    add max cap of parse station links from json

--- src/utils/privjsonstdlib.nim
+++ src/utils/privjsonstdlib.nim
@@ -14,7 +14,10 @@ proc loadStationStdLib*(filePath: string): tuple[names, urls: seq[string]] =
         let stations = jsonData["stations"]
         result = (names: newSeq[string](), urls: newSeq[string]())
 
+        var countCap: uint8 
         for stationName, stationUrlNode in stations.pairs:
+              if countCap == 20: break #exceed too much
+          
           let stationNameStr = stationName
           let stationUrlStr = stationUrlNode.getStr
 
@@ -23,7 +26,8 @@ proc loadStationStdLib*(filePath: string): tuple[names, urls: seq[string]] =
 
           result.names.add(stationNameStr)
           result.urls.add(normalizedUrl)
-
+          countCap += 1
+        
         # Validate station names (only if it's not the quotes file)
         #if not filePath.endsWith("qoute.json"):
         #  validateLengthStationName(result.names, filePath)
