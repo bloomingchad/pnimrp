3dbe6e99eb8ac7fde65327acfc3f9a94d18c20d0
bloomingchad <antonloyed007@gmail.com>
2025-03-21 20:14:28 +0530

    run through nimpretty for rest of files

--- src/ui/playerui.nim
+++ src/ui/playerui.nim
@@ -20,7 +20,7 @@ when not defined(simple):
     ../ui/[
       scroll,
       animation,
-     ]
+      ]
 
 proc editBadFileHint(config: MenuConfig, extraMsg = "") =
   if extraMsg != "": warn(extraMsg)
@@ -52,161 +52,163 @@ func milSecToSec(ms: int): float = ms / 1000
 func getFileFormat(ctx: ptr Handle): string =
   # Query the `file-format` property
   var format: cstring = getPropertyString(ctx, "file-format")
-    
+
   if format != nil:
     result = $format  # Convert cstring to Nim string
     free(format)  # Free the allocated cstring
   else:
-    result = "unknown"  # Fallback if the property is not available
+    result = "unknown" # Fallback if the property is not available
 
 proc playStation*(config: MenuConfig) =
-    ## Plays a radio station and handles user input for playback control.
-    #try:
-    if config.stationUrl == "":
+  ## Plays a radio station and handles user input for playback control.
+  #try:
+  if config.stationUrl == "":
+    editBadFileHint(config)
+    return
+  #defer: illwillDeinit()
+  #illwillInit(false)
+
+  # Validate the link
+  try:
+    initCheckingStationNotice()
+    if not validateLink(config.stationUrl).isValid:
       editBadFileHint(config)
       return
+    finishCheckingStationNotice()
+  except Exception:
+    editBadFileHint(config)
+    return
 
-    # Validate the link
-    try:
-      initCheckingStationNotice()
-      if not validateLink(config.stationUrl).isValid:
-        editBadFileHint(config)
-        return
-      finishCheckingStationNotice()
-    except Exception:
-      editBadFileHint(config)
-      return
+  var state = PlayerState(isPaused: false, isMuted: false, volume: lastVolume) # Use lastVolume
+  var isObserving = false
+  var coreIdleCounter: uint8
+  var playlistFirstPass = false
+  var scrollOffset: int = 0
+  var lastWidth: int = 0
+  var fullTitle: string # Declare fullTitle here
+  const mpvEventLoopTimeout = KeyTimeout.milSecToSec
 
-    var state = PlayerState(isPaused: false, isMuted: false, volume: lastVolume)  # Use lastVolume
-    var isObserving = false
-    var coreIdleCounter: uint8
-    var playlistFirstPass = false
-    var scrollOffset: int = 0
-    var lastWidth: int = 0
-    var fullTitle: string # Declare fullTitle here
-    const mpvEventLoopTimeout = KeyTimeout.milSecToSec
+  when not defined(simple):
+    var metadata = initTable[string, string](8) # Declare metadata here
+    var scrollCounter: int = 0
+    var animationCounter: int = 0 # Counter for animation updates
+
+
+  allocateJobMpv(config.stationUrl)
+  var event = mpvCtx.waitEvent()
+
+
+  # Draw the initial player UI
+  drawPlayerUI(config.stationName, "Loading...", currentStatusEmoji(currentStatus(state)), state.volume)
+  showFooter(isPlayerUI = true)
+  when defined(simple):
+    fullTitle = fullTitle.truncateMe()
+
+  while true:
+    if not state.isPaused:
+      event = mpvCtx.waitEvent(mpvEventLoopTimeout)
+
+    # Handle playback events
+    if event.eventID in {IDPlaybackRestart} and not isObserving:
+      when not defined(simple): mpvCtx.observeMetadata()
+      else: mpvCtx.observeMediaTitle()
+
+      isObserving = true
+
+    if event.eventID in {IDEventPropertyChange}:
+      state.currentSong = mpvCtx.getCurrentMediaTitle()
+      fullTitle = state.currentSong # Assign to fullTitle
+      updateCurrentSongPlayerUI(state.currentSong)
+      setCursorXPos 0
+      when not defined(simple):
+        globalMetadata = updateMetadataUI(config, mpvCtx, state)
+
 
     when not defined(simple):
-      var metadata = initTable[string, string](8) # Declare metadata here
-      var scrollCounter: int = 0
-      var animationCounter: int = 0  # Counter for animation updates
-    
+      # Increment the animation counter every 25ms (getKeyWithTimeout interval)
+      animationCounter += 1
 
-    allocateJobMpv(config.stationUrl)
-    var event = mpvCtx.waitEvent()
-
-
-    # Draw the initial player UI
-    drawPlayerUI(config.stationName, "Loading...", currentStatusEmoji(currentStatus(state)), state.volume)
-    showFooter(isPlayerUI = true)
-    when defined(simple):
-      fullTitle = fullTitle.truncateMe()
-
-    while true:
-      if not state.isPaused:
-        event = mpvCtx.waitEvent(mpvEventLoopTimeout)
-
-      # Handle playback events
-      if event.eventID in {IDPlaybackRestart} and not isObserving:
-        when not defined(simple): mpvCtx.observeMetadata()
-        else: mpvCtx.observeMediaTitle()
-
-        isObserving = true
-
-      if event.eventID in {IDEventPropertyChange}:
-        state.currentSong = mpvCtx.getCurrentMediaTitle()
-        fullTitle = state.currentSong # Assign to fullTitle
-        updateCurrentSongPlayerUI(state.currentSong)
-        setCursorXPos 0
-        when not defined(simple):
-          globalMetadata = updateMetadataUI(config, mpvCtx, state)
-
-
-      when not defined(simple):
-        # Increment the animation counter every 25ms (getKeyWithTimeout interval)
-        animationCounter += 1
-
-        # Check if it's time to update the animation (1350ms / 50ms = 54 iterations)
-        if animationCounter == 27:
-          updateAnimationOnly(currentStatusEmoji(currentStatus(state)), state.currentSong, animationCounter)
-          animationCounter = 0  # Reset the counter
+      # Check if it's time to update the animation (1350ms / 50ms = 54 iterations)
+      if animationCounter == 27:
+        updateAnimationOnly(currentStatusEmoji(currentStatus(state)), state.currentSong, animationCounter)
+        animationCounter = 0 # Reset the counter
 
 
         # Scrolling Logic
-        if scrollCounter == 11:
-         if state.currentSong.checkIfTooLongForUI():
+      if scrollCounter == 11:
+        if state.currentSong.checkIfTooLongForUI():
           scrollTextOnce(fullTitle, scrollOffset, termWidth, startingX) # Corrected call
           if fullTitle.len > termWidth - startingX:
             scrollOffset += 1
           scrollCounter = 0
-        else:
-          scrollCounter += 1
+      else:
+        scrollCounter += 1
 
-      # Periodic checks
-      if coreIdleCounter >= CheckIdleInterval:
-        if mpvCtx.isIdle():
-          handlePlayerError("Player core idle", config)
-          break
+    # Periodic checks
+    if coreIdleCounter >= CheckIdleInterval:
+      if mpvCtx.isIdle():
+        handlePlayerError("Player core idle", config)
+        break
 
-        if event.eventID in {IDEndFile, IDShutdown}:
-          if config.stationUrl.isValidPlaylistUrl():
-            if playlistFirstPass:
-              handlePlayerError("End of playlist reached", config)
-              break
-            playlistFirstPass = true
-          else:
-            handlePlayerError("Stream ended", config)
+      if event.eventID in {IDEndFile, IDShutdown}:
+        if config.stationUrl.isValidPlaylistUrl():
+          if playlistFirstPass:
+            handlePlayerError("End of playlist reached", config)
             break
-        coreIdleCounter = 0
-      inc coreIdleCounter
-
-      # Handle user input
-      case getKeyWithTimeout(KeyTimeout):
-        of Key.P:
-          state.isPaused = not state.isPaused
-          mpvCtx.pause(state.isPaused)
-          updatePlayMutedStatePlayerUI(currentStatusEmoji(currentStatus(state)))
-
-        of Key.M:
-          state.isMuted = not state.isMuted
-          mpvCtx.mute(state.isMuted)
-          updatePlayMutedStatePlayerUI(currentStatusEmoji(currentStatus(state)))
-
-        of Key.Slash, Key.Plus:
-          state.volume = min(state.volume + VolumeStep, MaxVolume)
-          cE mpvCtx.setProperty("volume", fmtInt64, addr state.volume)
-          lastVolume = state.volume
-          updateVolumePlayerUI(state.volume)
-
-        of Key.Asterisk, Key.Minus:
-          state.volume = max(state.volume - VolumeStep, MinVolume)
-          cE mpvCtx.setProperty("volume", fmtInt64, addr state.volume)
-          lastVolume = state.volume
-          updateVolumePlayerUI(state.volume)
-
-        of Key.R, Key.BackSpace:
-          if not state.isPaused:
-            cleanupPlayer(mpvCtx)
-          stopCurrentJob()
-          break
-
-        of Key.Q, Key.Escape:
-          cleanupPlayer(mpvCtx)
-          exit(mpvCtx, state.isPaused)
-
-        of Key.L:  # New key binding for "Like" action
-          if state.currentSong != "":
-            appendToLikedSongs()
-          else:
-            warn("song field is currently empty")
-
-        of Key.None:
-          continue
-
+          playlistFirstPass = true
         else:
-          showInvalidChoice()
+          handlePlayerError("Stream ended", config)
+          break
+      coreIdleCounter = 0
+    inc coreIdleCounter
 
-  #except Exception:
+    # Handle user input
+    case getKeyWithTimeout(KeyTimeout):
+      of Key.P:
+        state.isPaused = not state.isPaused
+        mpvCtx.pause(state.isPaused)
+        updatePlayMutedStatePlayerUI(currentStatusEmoji(currentStatus(state)))
+
+      of Key.M:
+        state.isMuted = not state.isMuted
+        mpvCtx.mute(state.isMuted)
+        updatePlayMutedStatePlayerUI(currentStatusEmoji(currentStatus(state)))
+
+      of Key.Slash, Key.Plus:
+        state.volume = min(state.volume + VolumeStep, MaxVolume)
+        cE mpvCtx.setProperty("volume", fmtInt64, addr state.volume)
+        lastVolume = state.volume
+        updateVolumePlayerUI(state.volume)
+
+      of Key.Asterisk, Key.Minus:
+        state.volume = max(state.volume - VolumeStep, MinVolume)
+        cE mpvCtx.setProperty("volume", fmtInt64, addr state.volume)
+        lastVolume = state.volume
+        updateVolumePlayerUI(state.volume)
+
+      of Key.R, Key.BackSpace:
+        if not state.isPaused:
+          cleanupPlayer(mpvCtx)
+        stopCurrentJob()
+        break
+
+      of Key.Q, Key.Escape:
+        cleanupPlayer(mpvCtx)
+        exit(mpvCtx, state.isPaused)
+
+      of Key.L: # New key binding for "Like" action
+        if state.currentSong != "":
+          appendToLikedSongs()
+        else:
+          warn("song field is currently empty")
+
+      of Key.None:
+        continue
+
+      else:
+        showInvalidChoice()
+
+#except Exception:
   #  let fileHint = if config.currentSubsection != "": config.currentSubsection else: config.currentSection
   #  warn("An error occurred during playback. Edit the station list in: " & fileHint & ".json")
   #  cleanupPlayer(mpvCtx)
--- src/utils/privjsonstdlib.nim
+++ src/utils/privjsonstdlib.nim
@@ -3,52 +3,52 @@ from ../link/linkbase import normalizeUrl
 import utilstypes
 
 proc loadStationStdLib*(filePath: string): tuple[names, urls: seq[string]] =
-        let jsonData = parseJson(readFile(filePath))
+  let jsonData = parseJson(readFile(filePath))
 
-        if not jsonData.hasKey("stations"):
-          raise newException(JSONParseError, "Missing 'stations' key in JSON file.")
+  if not jsonData.hasKey("stations"):
+    raise newException(JSONParseError, "Missing 'stations' key in JSON file.")
 
-        let stations = jsonData["stations"]
-        result = (names: newSeq[string](), urls: newSeq[string]())
+  let stations = jsonData["stations"]
+  result = (names: newSeq[string](), urls: newSeq[string]())
 
-        var countCap: uint8 
-        for stationName, stationUrlNode in stations.pairs:
-          if countCap == 20: break #exceed too much
-          
-          let stationNameStr = stationName
-          let stationUrlStr = stationUrlNode.getStr
+  var countCap: uint8
+  for stationName, stationUrlNode in stations.pairs:
+    if countCap == 20: break #exceed too much
 
-          # Normalize the URL using linkbase.normalizeUrl
-          let normalizedUrl = normalizeUrl(stationUrlStr)
+    let stationNameStr = stationName
+    let stationUrlStr = stationUrlNode.getStr
 
-          result.names.add(stationNameStr)
-          result.urls.add(normalizedUrl)
-          countCap += 1
-        
-        # Validate station names (only if it's not the quotes file)
-        #if not filePath.endsWith("qoute.json"):
-        #  validateLengthStationName(result.names, filePath)
+    # Normalize the URL using linkbase.normalizeUrl
+    let normalizedUrl = normalizeUrl(stationUrlStr)
+
+    result.names.add(stationNameStr)
+    result.urls.add(normalizedUrl)
+    countCap += 1
+
+  # Validate station names (only if it's not the quotes file)
+  #if not filePath.endsWith("qoute.json"):
+  #  validateLengthStationName(result.names, filePath)
 
 proc loadQuotesStdLib*(filePath: string): QuoteData =
-    ## Loads and validates quotes from a JSON file.
-    ## Raises `UIError` if the quote data is invalid.
-    let jsonData = parseJson(readFile(filePath))
+  ## Loads and validates quotes from a JSON file.
+  ## Raises `UIError` if the quote data is invalid.
+  let jsonData = parseJson(readFile(filePath))
 
-    # Check if the JSON is an object (for the new format)
-    if jsonData.kind != JObject:
-        raise newException(InvalidDataError, "Invalid JSON format: expected an object.")
+  # Check if the JSON is an object (for the new format)
+  if jsonData.kind != JObject:
+    raise newException(InvalidDataError, "Invalid JSON format: expected an object.")
 
-    result = QuoteData(quotes: newSeqOfCap[string](32), authors: newSeqOfCap[string](32))  # Initialize empty sequences
+  result = QuoteData(quotes: newSeqOfCap[string](32), authors: newSeqOfCap[string](32)) # Initialize empty sequences
 
     # Iterate over the key-value pairs in the JSON object
-    for quote, author in jsonData.pairs:
-        result.quotes.add(quote)        # Add the quote (key)
-        result.authors.add(author.getStr)  # Add the author (value, converted to string)
+  for quote, author in jsonData.pairs:
+    result.quotes.add(quote) # Add the quote (key)
+    result.authors.add(author.getStr) # Add the author (value, converted to string)
     
     # Validate quotes and authors
-    for i in 0 ..< result.quotes.len:
-      if result.quotes[i].len == 0:
-        raise newException(InvalidDataError, "Empty quote found at index " & $i)
-      if result.authors[i].len == 0:
-        raise newException(InvalidDataError, "Empty author found for quote at index " & $i)
+  for i in 0 ..< result.quotes.len:
+    if result.quotes[i].len == 0:
+      raise newException(InvalidDataError, "Empty quote found at index " & $i)
+    if result.authors[i].len == 0:
+      raise newException(InvalidDataError, "Empty author found for quote at index " & $i)
 
