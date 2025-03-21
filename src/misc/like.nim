8e91be215549a1efdcc7da072677d777cc5e35d7
bloomingchad <antonloyed007@gmail.com>
2025-03-21 21:15:02 +0530

    move like to its own module

---
+++ src/misc/like.nim
@@ -0,0 +1,24 @@
+import terminal, ../utils/utils, ../audio/player
+
+proc appendToLikedSongs* =
+  ## Appends a song to the likedSongs.txt file.
+  const likedSongsFile = "likedSongs.txt"
+  try:
+    # Open the file in append mode (creates the file if it doesn't exist)
+    let file = open(likedSongsFile, fmAppend)
+    defer: file.close()
+
+    # Append the song and a newline
+    file.writeLine(fullMediaTitle)
+    cursorDown 5
+    warn("Song added to likedSongs.txt") # Notify the user
+    cursorUp()
+    eraseLine()
+    cursorUp 5
+
+  except IOError as e:
+    warn("Failed to save song to likedSongs.txt: " & e.msg)
+
+  finally:
+    stdout.flushFile()
+
--- src/utils/utils.nim
+++ src/utils/utils.nim
@@ -43,29 +43,6 @@ proc validateLengthStationName*(result: seq[string], filePath: string, maxLength
         sleep(400) # Pause for 400ms after displaying the warning
         warnCount += 1
 
-
-proc appendToLikedSongs* =
-  ## Appends a song to the likedSongs.txt file.
-  const likedSongsFile = "likedSongs.txt"
-  try:
-    # Open the file in append mode (creates the file if it doesn't exist)
-    let file = open(likedSongsFile, fmAppend)
-    defer: file.close()
-
-    # Append the song and a newline
-    file.writeLine(fullMediaTitle)
-    cursorDown 5
-    warn("Song added to likedSongs.txt") # Notify the user
-    cursorUp()
-    eraseLine()
-    cursorUp 5
-
-  except IOError as e:
-    warn("Failed to save song to likedSongs.txt: " & e.msg)
-
-  finally:
-    stdout.flushFile()
-
 proc checkIfTooLongMagic*: int = terminalWidth() - "  Now Playing: ".len - 6
 
 proc checkIfTooLongForUI*(str: string): bool =
