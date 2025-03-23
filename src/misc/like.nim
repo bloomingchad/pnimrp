3bf250ee62085d9a8b3fbe9da679f71f4158f02f
bloomingchad <antonloyed007@gmail.com>
2025-03-23 21:06:32 +0530

    feat: like: add section name to liked songs

--- src/misc/like.nim
+++ src/misc/like.nim
@@ -3,7 +3,7 @@ import
 
   ../utils/utils, ../audio/player
 
-proc appendToLikedSongs* =
+proc appendToLikedSongs*(sectionName: string) =
   ## Appends a song to the likedSongs.txt file.
   const likedSongsFile = "likedSongs.txt"
   try:
@@ -12,7 +12,13 @@ proc appendToLikedSongs* =
     defer: file.close()
 
     # Append the song and a newline
-    file.writeLine("[" & getDateStr() & "][" & getClockStr() & "] " & fullMediaTitle)
+
+    file.writeLine(
+      "[" & getDateStr()  & "]"  &
+      "[" & getClockStr() & "] " &
+            sectionName   & " " &
+      fullMediaTitle
+      )
     cursorDown 5
     warn("Song added to likedSongs.txt") # Notify the user
     cursorUp()
