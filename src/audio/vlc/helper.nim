9538a7e0945c838716bd823b615ccd322995d337
bloomingchad <antonloyed007@gmail.com>
2025-04-11 14:40:45 +0530

    feat: add vlc backend with idomatic helper and example

---
+++ src/audio/vlc/helper.nim
@@ -0,0 +1,96 @@
+import libvlc
+
+type VlcError* = object of CatchableError
+
+var playerCurrentSituation*: libvlc.state
+var currentVol*: cint
+
+type libvlcHandle* = object
+  ctx*: ptr libvlc.instance
+  mediaDscptr*: ptr libvlc.media
+  mediaPlayerCtx*: ptr libvlc.mediaPlayer
+
+proc checkError*(status: cint, msg: cstring) =
+  if status == -1:
+    raise newException(VlcError, $msg)
+
+template cE*(status: cint, msg: cstring) =
+  checkError(status, msg)
+
+using Handle: ref libvlcHandle
+
+proc playPlayer*(Handle) =
+  let msg: cstring = "play player"
+  cE(libvlc.mediaPlayerPlay(Handle.mediaPlayerCtx), msg)
+
+template resume*(Handle) =
+  playPlayer(Handle)
+
+proc pausePlayer*(Handle) =
+  libvlc.mediaPlayerPause(Handle.mediaPlayerCtx)
+
+proc setVolume*(Handle; volume: int) =
+  let msg: cstring = "set volume"
+  cE(libvlc.audioSetVolume(Handle.mediaPlayerCtx, cint(volume)), msg)
+
+template volumeChange*(Handle; inc: bool) =
+  when inc:
+    setVolume(Handle, currentVol + 5)
+  else:
+    setVolume(Handle, currentVol - 5)
+
+var volumeBeforeMuted*: int
+
+proc muteVolume*(Handle) =
+  volumeBeforeMuted = libvlc.audioGetVolume(Handle.mediaPlayerCtx)
+  setVolume(Handle, 0)
+
+proc unmuteVolume*(Handle) =
+  setVolume(Handle, volumeBeforeMuted)
+  volumeBeforeMuted = -1
+
+template volumeUp*(Handle) =
+  volumeChange(Handle, true)
+
+template volumeDown*(Handle) =
+  volumeChange(Handle, false)
+
+proc setVolumeOfBellRelativeToMainCtx*(Handle) =
+  var relativeVol: int = int(1.5 * float(currentVol))
+  setVolume(Handle, relativeVol)
+
+proc stopPlayer*(Handle) =
+  libvlc.mediaPlayerStop(Handle.mediaPlayerCtx)
+
+proc isIdle*(Handle): bool =
+  playerCurrentSituation = libvlc.mediaPlayerGetState(Handle.mediaPlayerCtx)
+  if playerCurrentSituation in [libvlc.Ended, libvlc.Error, libvlc.Stopped]:
+    return true
+  else:
+    return false
+
+## auto observeMediaTitle() {}
+
+proc getCurrentMediaTitleVlc*(Handle): string =
+  return $libvlc.mediaGetMeta(Handle.mediaDscptr, libvlc.metaNowPlaying)
+
+proc initNewCtx*(Handle): ref libvlc_handle =
+  var ctx = libvlc.new(0, allocCStringArray [])
+  if ctx.isNil:
+    raise newException(VlcError, "Failed to create libvlc instance")
+
+  Handle.ctx = ctx
+  return Handle
+
+proc deinitPlayer*(Handle) =
+  libvlc.mediaPlayerRelease(Handle.mediaPlayerCtx)
+  libvlc.mediaRelease(Handle.mediaDscptr)
+  libvlc.release(Handle.ctx)
+
+proc allocateJobVlc*(Handle; url: string) =
+  Handle.mediaDscptr = libvlc.mediaNewLocation(Handle.ctx, cstring(url))
+  Handle.media_player_ctx = libvlc.mediaPlayerNewFromMedia(Handle.mediaDscptr)
+
+proc setAllyOptionsVlc*(Handle) =
+  libvlc.setUserAgent(Handle.ctx, "pnimrp/0.1", "pnimrp/0.1")
+  libvlc.setAppId(Handle.ctx, "pnimrp", "0.1", "")
