2b35d0fe179d88d6fb015b62d33d47d63386f2bd
bloomingchad <antonloyed007@gmail.com>
2025-04-13 02:58:54 +0530

    ffi: use object instead of pointer
      - there is no difference in behavior
      - yet the pointer code makes it void*
      - the object one makes it typedef struct T T;
         which is what we see as opaque pointers

--- src/audio/vlc/libvlc/base.nim
+++ src/audio/vlc/libvlc/base.nim
@@ -1,4 +1,4 @@
 type
-  instance*    = distinct pointer
-  media*       = distinct pointer
-  mediaPlayer* = distinct pointer
+  instance*    = distinct object
+  media*       = distinct object
+  mediaPlayer* = distinct object
