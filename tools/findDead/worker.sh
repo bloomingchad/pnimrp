69eeebc75475dc47cfdf212e9eba1cc8b7945b36
bloomingchad <antonloyed007@gmail.com>
2025-04-12 10:50:19 +0530

    finddead.worker: + curl timeout > 8 and - limit rate > 1K
      - the spec is to use GET on all servers
      - mediainfo only needs small size to know the metadata

--- tools/findDead/worker.sh
+++ tools/findDead/worker.sh
@@ -97,9 +97,9 @@ CURL_OPTS=(
     --silent
     -L
     --insecure
-    --max-time 7
+    --max-time 8
     --http0.9
-    --limit-rate 20K
+    --limit-rate 1K
     --connect-timeout 5
     --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
     --header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
