e4d8b479bccb786f16d876872655182e05663964
bloomingchad <antonloyed007@gmail.com>
2025-04-17 10:44:30 +0530

    feat: radiob: impl a proper lib with exmpl

--- src/api/radiob.nim
+++ src/api/radiob.nim
@@ -1,50 +1,57 @@
+import net, httpclient, json, strutils
 import std/cmdline
 
-import net, httpclient, json, strutils
+const apiEndPoint = "https://all.api.radio-browser.info/"
+const maxStationCap = 20
 
-let baseUrl = "https://de2.api.radio-browser.info/"
-
-let client =
-  newHttpClient(
-    sslContext=newContext(verifyMode=CVerifyNone),
-    userAgent="pnimrp/0.1"
-  )
-
-#let fileName = "house.json"
-let fileName = commandLineParams()[0] #"house.json"
-
-proc sanitizeStr(str: string): string =
+proc sanitizeEscapedQuotesFromRB(str: string): string =
   var tempStr = str
   tempStr.removePrefix("\"")
   tempStr.removeSuffix("\"")
   return tempStr
+
+template debug*: bool =
+  not defined(release) or
+    not defined(danger) or
+      defined(debug)
+
+template didFindInStr(res: int): bool = res != -1
+
+func sanitizeStationName(str: string): string =
+  var tempStr = str
+  var findFoundAtThisIndex: int = -1
+  var charToStartSearchingFrom: int
+  while true:
+    findFoundAtThisIndex = tempStr.find('_', start = charToStartSearchingFrom)
+    if findFoundAtThisIndex.didFindInStr():
+      tempStr.delete(findFoundAtThisIndex .. findFoundAtThisIndex)
+      charToStartSearchingFrom = findFoundAtThisIndex
+    else: break
+  return tempStr
+
+template skipThisLoopIfTooLong =
+  if ($name in nameItems):
+    echo "skipped reason in arr: ", name
+    echo "skipped reason in arr url: ", url
+    continue
+
+  if (name.getStr.len > 40):
+    echo "skipped name too long: ", name
+    echo "skipped reason in arr url: ", url
+    continue
+
+  if url.getStr.len > 70:
+    echo "skpped url too long: ", url
+    continue
 
 proc getStyleToProcessFromFileName(fileName: string): string =
   var styleFromFilename = fileName
   styleFromFileName.removeSuffix(".json")
   result = styleFromFilename
 
-let styleToProcess = getStyleToProcessFromFileName(fileName)
-
-const MAX_STATION_CAP = 20
-
-template skipThisLoopIfTooLong =
-  if ($name in nameItems) or (name.getStr.len > 40):
-    #echo "[SKIPPED] name: ", name
-    #echo "[SKIPPED] len: ", name.getStr.len
-    #echo "------------------"
-
-    continue
-
-  if (url.getStr.len > 70):
-    continue
-
-#echo baseUrl & "json/stations/bytag/" & styleToProcess & "/?hidebroken=true&limit=50"
-#quit()
-
-let e =
+proc apiRequest(client: HttpClient, styleToProcess: string): string =
   client.getContent(
-    baseUrl &
+    apiEndPoint &
     "json/stations/bytag/" &
     styleToProcess &
     "?" &
@@ -52,47 +59,55 @@ let e =
     "reverse=true"    & "&" &
     "hidebroken=true" & "&" &
     "limit=70"        & "&" &
-    "lastcheckok=1"        #& "&" &
-)
+    "lastcheckok=1")
 
-let c = e.parseJson()
-
-var nameItems: seq[string]
-var urlItems:  seq[string]
-
-var
-  name: JsonNode
-  url:  JsonNode
-
-for station in 0 .. (c.len - 1):
-  if nameItems.len == MAX_STATION_CAP: break
-  name = c[station]["name"]
-  url = c[station]["url"]
-
-  skipThisLoopIfTooLong()
-  #echo "name: ", name #, ": "
-  #echo "len: ", name.getStr.len
-  nameItems.add $name
-
-  #echo "url: ", url
-  urlItems.add $url
-  #echo "------------------"
-#echo c.pretty()
-
-proc constructAndWriteExpectedJsonFromNamesAndUrlItem(nameItems, urlItems: seq[string]): string =
+proc constructExpectedJsonFromNamesAndUrlItem(nameItems, urlItems: seq[string]): string =
   var jsonObject = %* {"stations": {}}
   var stationList = newJObject()
 
-  #var jsonStationsObject = jsonObject["stations"]
+  var cleanedStationName, cleanedStationUrl: string
   for key in 0 .. nameItems.len - 1:
-    #stationList.add(nameItems[key], %*urlItems[key])
-    var cleanedStationName = nameItems[key].sanitizeStr()
-    var cleanedStationUrl  =  urlItems[key].sanitizeStr()
+    cleanedStationName = nameItems[key].sanitizeEscapedQuotesFromRB().sanitizeStationName()
+    cleanedStationUrl  =  urlItems[key].sanitizeEscapedQuotesFromRB()
     stationList[cleanedStationName] = %* cleanedStationUrl
 
   jsonObject["stations"] = stationList
-
-    #.add(urlItems[key])
   return jsonObject.pretty
 
-echo constructAndWriteExpectedJsonFromNamesAndUrlItem(nameItems, urlItems)
+proc getFormattingJsonFromFileName*(fileName: string): string =
+  let client =
+    newHttpClient(
+      sslContext = newContext(verifyMode=CVerifyNone),
+      userAgent = "pnimrp/0.1"
+    )
+
+  var name, url:  JsonNode
+  var nameItems, urlItems: seq[string]
+
+  let styleToGoGet = getStyleToProcessFromFileName(fileName)
+  let resultJson = client.apiRequest(styleToGoGet).parseJson()
+
+  for station in 0 .. (resultJson.len - 1):
+    if nameItems.len == maxStationCap: break
+    name = resultJson[station]["name"]
+    url  = resultJson[station]["url"]
+
+    when not defined(release) or
+      not defined(danger):
+      skipThisLoopIfTooLong()
+    nameItems.add ($name).sanitizeStationName()
+
+    urlItems.add $url
+
+  return constructExpectedJsonFromNamesAndUrlItem(nameItems, urlItems)
+
+when isMainModule:
+  let paramList = commandLineParams()
+  var fileName: string
+  try:
+    fileName = paramList[0]
+  except IndexDefect:
+    stderr.writeLine "please enter genre next time"
+    echo "selecting default.."
+
+  echo getFormattingJsonFromFileName(fileName)
